9e4970b57e355c4886e09be7d161fb7b56875125
andrewDoing <andrewsdoing@gmail.com>
2024-12-19 12:39:36 -0600

    wip

--- README.md
+++ README.md
@@ -1,12 +1,35 @@
 # Lichess
 Collection of projects utilizing the Lichess API
 
+## Getting Started
+
+This project uses the uv package resolver and installer. To install the necessary packages, run the following command:
+
+```bash
+uv install
+```
+
+You may need to install `uv` first. You can do so by running the following command:
+
+```bash
+# install to macos packages (homebrew)
+brew install uv
+
+# install to windows packages
+choco install uv
+
+# install to python global packages, but isolated from a specific version
+pipx install uv
+```
+
+
+
 
 ## Project 1: Opening Finder
 
 - Project Goal - determine the best opening to play against an opponent.
 - Project Methodology -
-  
+
 **Files**
 - opening_finder.py is the initial attempt to find the best opening to play by sorting through all of the lost games out of the last 50 games and returning the most played openings with their percentage lost. Eventually I'll update this to some weighted average but I didn't sort by percentage because I thought outliers would be a problem.
 - chess_cheater.py is the 2nd attempt, taking the exact code from opening_finder but making a *very* simple locally hosted web app using flask, port 9000
--- chess_cheater.py
+++ chess_cheater.py
@@ -1,5 +1,9 @@
 import berserk
 from flask import Flask, request, render_template_string
+import timeit
+from collections import Counter
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from functools import partial
 
 # Setup Flask app
 app = Flask(__name__)
@@ -59,43 +63,110 @@ def index():
 
     if request.method == "POST":
         username = request.form["username"]
+
         # Get game data from Lichess
-        game_data = list(client.games.export_by_player(username, max=number_of_games))
+        # start timeit
+
+        # Code under measurement
+        # timeit is used to measure the time taken to get the game data
+        # partial is used to pass the username and number of games to the function, akin to a lambda
+        # list is used to convert the timeit object to a list
+        partial_export = partial(client.games.export_by_player, username, max=number_of_games)
+        game_data = list(timeit.timeit(partial_export, number=1))
+
+        # end timeit
 
         # Get IDs of games and lost games
-        id_list = [game['id'] for game in game_data]
-        lost_id_list = []
-        for game in game_data:
-            try:
-                winner = game['winner']
-                black_player_id = game['players']['black']['user']['id']
-                white_player_id = game['players']['white']['user']['id']
-                if (winner == 'black' and black_player_id == username) or (winner == 'white' and white_player_id == username):
-                    continue
-                else:
-                    lost_id_list.append(game['id'])
-            except:
-                pass
+        def get_lost_id_list(game_data):
+            id_list = [game['id'] for game in game_data]
+            lost_id_list = []
+            for game in game_data:
+                try:
+                    winner = game['winner']
+                    black_player_id = game['players']['black']['user']['id']
+                    white_player_id = game['players']['white']['user']['id']
+                    if (winner == 'black' and black_player_id == username) or (winner == 'white' and white_player_id == username):
+                        continue
+                    else:
+                        lost_id_list.append(game['id'])
+                except:
+                    pass
 
+            return lost_id_list, id_list
+
+        lost_id_list, id_list = get_lost_id_list(game_data)
+        print(f"Time taken to get game data: {end - start}")
+
+        # slow code zone
         # Gather openings for all games
-        all_openings = [client.games.export(i)['opening']['name'] for i in id_list]
-        all_opening_counts = {opening: all_openings.count(opening) for opening in set(all_openings)}
+        def gather_openings(game_data):
+            # use a set for opening names to avoid duplicates
+            all_openings = [
+                game['opening']['name']
+                for game in game_data
+                if 'opening' in game and 'name' in game['opening']
+            ]
+            return all_openings
 
-        real_games_played = len(all_openings)
+        # benchmark this code
+        # time = timeit.timeit(lambda: Counter(gather_openings(game_data)), number=1)
+        # print(f"Time taken to gather openings: {time}")
+
+        all_opening_counts = Counter(gather_openings(game_data))
+        real_games_played = all_opening_counts.elements()
+        # code pretty slow above here?
 
         # Gather lost openings
-        lost_openings = [client.games.export(i)['opening']['name'] for i in lost_id_list]
+        def gather_lost_openings(lost_id_list):
+            lost_openings = []
+            with ThreadPoolExecutor(max_workers=3) as executor:
+                futures = {executor.submit(client.games.export, game_id): game_id for game_id in lost_id_list}
+                for future in as_completed(futures):
+                    try:
+                        game = future.result()
+                        opening = game.get('opening', {}).get('name')
+                        if opening:
+                            lost_openings.append(opening)
+                    except Exception:
+                        pass
+            return lost_openings
+
+        # time to get lost openings
+        # time = timeit.timeit(lambda: gather_lost_openings(lost_id_list), number=1)
+        # print(f"Time taken to gather lost openings: {time}")
+
+        lost_openings = gather_lost_openings(lost_id_list)
         lost_opening_counts = {opening: lost_openings.count(opening) for opening in set(lost_openings)}
 
         # Calculate loss percentages
-        opening_percentages = {
-            opening: (
-                lost_opening_counts.get(opening, 0),
-                total_count,
-                round((lost_opening_counts.get(opening, 0) / total_count) * 100, 2)
-            )
-            for opening, total_count in all_opening_counts.items()
-        }
+
+        def get_opening_percentages(datum):
+            all_opening_counts, lost_opening_counts = datum
+            opening_percentages = {
+                opening: (
+                    lost_count := lost_opening_counts.get(opening, 0),
+                    total_count,
+                    round((lost_count / total_count) * 100, 2) if total_count > 0 else 0
+                )
+                for opening, total_count in all_opening_counts.items()
+            }
+            return opening_percentages
+        # here I pack the data needed into a tuple since a lambda can only take one argument
+        datum = (all_opening_counts, lost_opening_counts)
+
+        time = timeit.timeit(lambda: get_opening_percentages(datum), number=1)
+        print(f"Time taken to get opening percentages: {time}")
+
+        opening_percentages = get_opening_percentages(datum)
+
+        # opening_percentages = {
+        #     opening: (
+        #         lost_opening_counts.get(opening, 0),
+        #         total_count,
+        #         round((lost_opening_counts.get(opening, 0) / total_count) * 100, 2)
+        #     )
+        #     for opening, total_count in all_opening_counts.items()
+        # }
 
         # Sort by most played
         sorted_opening_percentages = sorted(opening_percentages.items(), key=lambda item: item[1][1], reverse=True)
